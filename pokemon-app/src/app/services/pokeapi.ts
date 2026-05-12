bd5271955e8c8eb2165586dfe337c867cfa3fc68
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2026-05-12 10:38:30 +0000

    implementazione servizio pokeapi con observable e chiamate http

--- pokemon-app/src/app/services/pokeapi.ts
+++ pokemon-app/src/app/services/pokeapi.ts
@@ -1,6 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import {
+  PokemonTypeResponse,
+  PokemonByTypeResponse,
+  PokemonDetail
+} from '../models/pokemon';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Pokeapi {}
+export class PokeapiService {
+
+  baseUrl = 'https://pokeapi.co/api/v2';
+
+  constructor(public http: HttpClient)
+  {
+
+  }
+
+  getTypes(): Observable<PokemonTypeResponse>
+  {
+    return this.http.get<PokemonTypeResponse>(
+      `${this.baseUrl}/type`
+    );
+  }
+
+  getPokemonByType(type: string): Observable<PokemonByTypeResponse>
+  {
+    return this.http.get<PokemonByTypeResponse>(
+      `${this.baseUrl}/type/${type}`
+    );
+  }
+
+  getPokemonDetail(name: string): Observable<PokemonDetail>
+  {
+    return this.http.get<PokemonDetail>(
+      `${this.baseUrl}/pokemon/${name}`
+    );
+  }
+
+}
