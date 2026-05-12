5aa2338da8a5b10ea3aef6fd76fa647c0fad1994
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2026-05-12 10:40:03 +0000

    visualizzazione lista pokemon per categoria tramite route parameter

--- pokemon-app/src/app/components/pokemon-list/pokemon-list.ts
+++ pokemon-app/src/app/components/pokemon-list/pokemon-list.ts
@@ -1,9 +1,67 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, ParamMap, RouterModule } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+import { PokeapiService } from '../../services/pokeapi.service';
+
+import {
+  PokemonByTypeResponse
+} from '../../models/pokemon';
+
 @Component({
   selector: 'app-pokemon-list',
-  imports: [],
-  templateUrl: './pokemon-list.html',
-  styleUrl: './pokemon-list.css',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './pokemon-list.component.html',
+  styleUrl: './pokemon-list.component.css'
 })
-export class PokemonList {}
+export class PokemonListComponent {
+
+  data!: PokemonByTypeResponse;
+
+  loading: boolean = false;
+
+  o!: Observable<PokemonByTypeResponse>;
+
+  typeName: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    public pokeService: PokeapiService
+  )
+  {
+    this.route.paramMap.subscribe(this.getRouterParam);
+  }
+
+  getRouterParam = (params: ParamMap) =>
+  {
+    let type = params.get('name');
+
+    if(type)
+    {
+      this.typeName = type;
+
+      this.getPokemon(type);
+    }
+  }
+
+  getPokemon(type: string): void
+  {
+    this.loading = true;
+
+    this.o = this.pokeService.getPokemonByType(type);
+
+    this.o.subscribe(this.getData);
+  }
+
+  getData = (d: PokemonByTypeResponse) =>
+  {
+    this.data = d;
+
+    this.loading = false;
+  }
+
+}
