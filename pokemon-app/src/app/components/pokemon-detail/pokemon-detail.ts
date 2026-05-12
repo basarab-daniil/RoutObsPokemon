1d223d32353031b8926143ed2d4b7cdd53a4e3b1
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2026-05-12 10:40:42 +0000

    implementazione pagina dettaglio pokemon con observable e route params

--- pokemon-app/src/app/components/pokemon-detail/pokemon-detail.ts
+++ pokemon-app/src/app/components/pokemon-detail/pokemon-detail.ts
@@ -1,9 +1,62 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+import { PokeapiService } from '../../services/pokeapi.service';
+
+import { PokemonDetail } from '../../models/pokemon';
+
 @Component({
   selector: 'app-pokemon-detail',
-  imports: [],
-  templateUrl: './pokemon-detail.html',
-  styleUrl: './pokemon-detail.css',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pokemon-detail.component.html',
+  styleUrl: './pokemon-detail.component.css'
 })
-export class PokemonDetail {}
+export class PokemonDetailComponent {
+
+  data!: PokemonDetail;
+
+  loading: boolean = false;
+
+  o!: Observable<PokemonDetail>;
+
+  constructor(
+    private route: ActivatedRoute,
+    public pokeService: PokeapiService
+  )
+  {
+    this.route.paramMap.subscribe(this.getRouterParam);
+  }
+
+  getRouterParam = (params: ParamMap) =>
+  {
+    let name = params.get('name');
+
+    if(name)
+    {
+      this.getPokemon(name);
+    }
+  }
+
+  getPokemon(name: string): void
+  {
+    this.loading = true;
+
+    this.o = this.pokeService.getPokemonDetail(name);
+
+    this.o.subscribe(this.getData);
+  }
+
+  getData = (d: PokemonDetail) =>
+  {
+    this.data = d;
+
+    this.loading = false;
+  }
+
+}
