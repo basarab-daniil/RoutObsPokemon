2eafdc0761111b73a1e42bedd73732e25a656797
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2026-05-12 10:36:02 +0000

    aggiunta layout principale con router outlet

--- pokemon-app/src/app/app.ts
+++ pokemon-app/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, RouterModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class App {
-  protected readonly title = signal('pokemon-app');
+export class AppComponent {
+
 }
