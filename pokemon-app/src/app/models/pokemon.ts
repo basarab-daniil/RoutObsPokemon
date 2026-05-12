7058d1d117e0e84a71a6b495de560d75b8affefa
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2026-05-12 10:37:43 +0000

    aggiunta modelli dati pokemon

---
+++ pokemon-app/src/app/models/pokemon.ts
@@ -0,0 +1,34 @@
+export interface PokemonTypeResponse
+{
+    results: PokemonType[];
+}
+
+export interface PokemonType
+{
+    name: string;
+    url: string;
+}
+
+export interface PokemonByTypeResponse
+{
+    pokemon: PokemonEntry[];
+}
+
+export interface PokemonEntry
+{
+    pokemon: Pokemon;
+}
+
+export interface Pokemon
+{
+    name: string;
+    url: string;
+}
+
+export interface PokemonDetail
+{
+    name: string;
+    height: number;
+    weight: number;
+    base_experience: number;
+}
