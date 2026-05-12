e77d4a016c407396f5f7b171085fe0712b06fef4
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2026-05-12 10:39:19 +0000

    visualizzazione lista categorie pokemon tramite http get

--- pokemon-app/src/app/components/types/types.ts
+++ pokemon-app/src/app/components/types/types.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+import { PokeapiService } from '../../services/pokeapi.service';
+
+import {
+  PokemonType,
+  PokemonTypeResponse
+} from '../../models/pokemon';
 
 @Component({
   selector: 'app-types',
-  imports: [],
-  templateUrl: './types.html',
-  styleUrl: './types.css',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './types.component.html',
+  styleUrl: './types.component.css'
 })
-export class Types {}
+export class TypesComponent implements OnInit {
+
+  data!: PokemonTypeResponse;
+
+  loading: boolean = false;
+
+  o!: Observable<PokemonTypeResponse>;
+
+  constructor(public pokeService: PokeapiService)
+  {
+
+  }
+
+  ngOnInit(): void
+  {
+    this.getTypes();
+  }
+
+  getTypes(): void
+  {
+    this.loading = true;
+
+    this.o = this.pokeService.getTypes();
+
+    this.o.subscribe(this.getData);
+  }
+
+  getData = (d: PokemonTypeResponse) =>
+  {
+    this.data = d;
+
+    this.loading = false;
+  }
+
+}
