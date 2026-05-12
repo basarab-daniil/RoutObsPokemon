c3a7c5db3deed3f5dd8ff2680ddbf16919472e6a
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2026-05-12 10:35:05 +0000

    configurazione routing applicazione pokemon

--- pokemon-app/src/app/app.routes.ts
+++ pokemon-app/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { TypesComponent } from './components/types/types.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'types',
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'types',
+        component: TypesComponent
+    },
+
+    {
+        path: 'type/:name',
+        component: PokemonListComponent
+    },
+
+    {
+        path: 'pokemon/:name',
+        component: PokemonDetailComponent
+    }
+];
